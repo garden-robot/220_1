5b7643549fae09cfbda79d82bbd026af21fcce4b
Flanzenberg <14jrb3@queensu.ca>
2016-10-17 22:36:20 -0400

    Add files via upload

---
+++ permissionResetter.sh
@@ -0,0 +1,21 @@
+#Josh Burak - 10169785
+#Kat Pinto (Zwach) - 10168919
+#Dhanish Chinniah - 10170731
+
+#Question 4, Part 2
+#Psuedo!
+
+#Unsure how to add arguments?
+
+#Check first arg with if/else statement, either -s or -r
+
+#if -s, check current file permissions
+#grep through the saved permission file to extract old permissions
+#echo the permissions in an organied list
+
+#if -r, then iterate through loops to interpret the saved permissions
+#change the saved permissions to binary operators
+#apply the binary operators to the file with chmod
+#echo that the change has been made
+
+echo This is a psuedo code file, so it runs nothing!
